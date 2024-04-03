7d824de4bab894e749b6cc6076dd235dec74b272
aishik <kirtuaishik@gmail.com>
2024-04-03 17:12:49 +0530

    Added Coroutines to Network Calls

--- app/src/main/java/com/textsdev/cleanweatherapp_mvvm/data/repository/WeatherRepositoryImpl.kt
+++ app/src/main/java/com/textsdev/cleanweatherapp_mvvm/data/repository/WeatherRepositoryImpl.kt
@@ -4,40 +4,44 @@ import com.textsdev.cleanweatherapp_mvvm.BuildConfig
 import com.textsdev.cleanweatherapp_mvvm.data.model.CurrentWeatherOfflineModel
 import com.textsdev.cleanweatherapp_mvvm.data.model.WeatherForecastOfflineModel
 import com.textsdev.cleanweatherapp_mvvm.network.WeatherApiService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import kotlin.math.roundToInt
 
 class WeatherRepositoryImpl(private val apiService: WeatherApiService) : WeatherRepository {
     override suspend fun getCurrentWeather(cityName: String): WeatherResult<CurrentWeatherOfflineModel> {
-        val response = apiService.getCurrentWeather(cityName, BuildConfig.API_KEY)
-        if (response.isSuccessful) {
-            val weatherResponse = response.body()
-            // Assuming WeatherResponse contains necessary data
-            return WeatherResult.Success(
-                CurrentWeatherOfflineModel(
-                    cityName = weatherResponse?.name ?: "",
-                    temperature = weatherResponse?.main?.temp ?: 0.0,
+        return withContext(Dispatchers.IO) {
+            val response = apiService.getCurrentWeather(cityName, BuildConfig.API_KEY)
+            if (response.isSuccessful) {
+                val weatherResponse = response.body()
+                WeatherResult.Success(
+                    CurrentWeatherOfflineModel(
+                        cityName = weatherResponse?.name ?: "",
+                        temperature = weatherResponse?.main?.temp ?: 0.0
+                    )
                 )
-            )
-        } else {
-            return WeatherResult.Error("Error occurred while fetching weather data: ${response.code()}")
+            } else {
+                WeatherResult.Error("Error occurred while fetching weather data: ${response.code()}")
+            }
         }
     }
 
     override suspend fun getWeatherForecast(cityName: String): WeatherResult<List<WeatherForecastOfflineModel>> {
-        val response = apiService.getWeatherForecast(cityName, BuildConfig.API_KEY)
-        return if (response.isSuccessful) {
-            val weatherResponse = response.body()
-            // Assuming WeatherResponse contains necessary data
-            val weatherForecastOfflineModelList: List<WeatherForecastOfflineModel> =
-                weatherResponse?.list?.map {
-                    WeatherForecastOfflineModel(
-                        date = it.dt,
-                        temp = it.temp.day.roundToInt(),
-                    )
-                } ?: emptyList()
-            WeatherResult.Success(weatherForecastOfflineModelList)
-        } else {
-            WeatherResult.Error("Error occurred while fetching weather data: ${response.code()}")
+        return withContext(Dispatchers.IO) {
+            val response = apiService.getWeatherForecast(cityName, BuildConfig.API_KEY)
+            if (response.isSuccessful) {
+                val weatherResponse = response.body()
+                val weatherForecastOfflineModelList: List<WeatherForecastOfflineModel> =
+                    weatherResponse?.list?.map {
+                        WeatherForecastOfflineModel(
+                            date = it.dt,
+                            temp = it.temp.day.roundToInt(),
+                        )
+                    } ?: emptyList()
+                WeatherResult.Success(weatherForecastOfflineModelList)
+            } else {
+                WeatherResult.Error("Error occurred while fetching weather data: ${response.code()}")
+            }
         }
     }
 }
